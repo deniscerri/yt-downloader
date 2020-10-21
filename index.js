024e757f481949e7e3f0ba8e00913df2f29bc1fa
deniscerri <deniscerri@users.noreply.github.com>
2020-10-21 19:26:58 +0200

    Fixes

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(port, () =>{
 
 app.use(cors());
 
-app.use(express.static(application_root + "/"));
+app.use(express.static(application_root + "/public"));
 
 app.get('/download', async (req, res) =>{
   var URL = req.query.URL;
