77154abd2e1cc1dcab5835a1f5e07b5542456c47
deniscerri <64997243+deniscerri@users.noreply.github.com>
2020-10-21 17:20:11 +0200

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,19 @@
+var convertBtn = document.querySelector('.mp3-button');
+var convertBtn2 = document.querySelector('.mp4-button');
+var URLinput = document.querySelector('.URL-input');
+
+
+convertBtn.addEventListener("click", function(){
+  console.log(`URL:${URLinput.value}`);
+  sendURL(URLinput.value, 'mp3');
+})
+
+
+convertBtn2.addEventListener("click", function(){
+  console.log(`URL:${URLinput.value}`);
+  sendURL(URLinput.value, 'mp4');
+})
+
+function sendURL(URL,FILE){
+  window.location.href = `http://ytdown.deniscerri.repl.co/download?URL=${URL}&File=${FILE}`
+}
