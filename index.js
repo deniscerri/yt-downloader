519b8a2d620d117c0160c2c4ef2d5348e5995afc
deniscerri <deniscerri@users.noreply.github.com>
2020-10-22 23:47:33 +0200

    Making MP4 uploads have the highest quality
    Before, mp4 downloaded in 360p. Now since files that have really high quality are split into two parts for audio and video. Used FFMPEG to merge those and  delete them after merging. Piping the created video and after the download is finished, delete the merged video aswell. Since the mp4 portion too much code on its own, i split the function from the mp3.
    
    I should've used ffmpeg to transform m4a file to mp3 instead of just renaming. Next Update i guess

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const express = require('express')
 const fs = require('fs')
 const cors = require('cors');
 const ytdl = require('ytdl-core')
+const contentDisposition = require('content-disposition');
 
-const { promisify } = require('util')
-const getInfoVideo =  promisify(ytdl.getInfo)
+const ffmpeg = require('fluent-ffmpeg');
+
+
 
 
 const app = express();
 var application_root = __dirname
 
 var port = process.env.port || 8080
-
 
 app.listen(port, () =>{
   console.log('Server is working');
@@ -22,34 +23,93 @@ app.use(cors());
 
 app.use(express.static(application_root + "/public"));
 
-app.get('/download', async (req, res) =>{
-  var URL = req.query.URL;
-  var File = req.query.File;
-  
-  var code;
-  if(File === 'mp3'){
-    code = 'highestaudio';
-  }else{
-    code = 'highest';
-  }
 
+app.get('/download/MP3', async (req, res) =>{
+  var URL = req.query.URL;
+ 
   try {
 
-        const info = await getInfoVideo(URL.replace('https://www.youtube.com/watch?v=', ''))
+      let info = await ytdl.getInfo(URL)
 
-        res.writeHead(200, {
+      let filename = info.videoDetails.title+".mp3";
+      var title = info.videoDetails.title;
+
+      
+
+      res.writeHead(200, {
         'Content-Type': 'application/force-download',
-        'Content-disposition': `attachment; filename=${info.videoDetails.title}.${File}`
-        });
-
-        
+        'Content-disposition':contentDisposition(filename)});
 
 
-        ytdl(URL,{
-          quality: `${code}`
-        }).pipe(res)
-
+           // Get audio and video stream going
+      const audio = ytdl(URL, { filter: 'audioonly', quality: 'highestaudio' })
+      .pipe(res);
+       
+  
     } catch (err) {
-        res.status(500).json(err)
+        console.log(err);
     }
+
+
+})
+
+app.get('/download/MP4', async (req, res) =>{
+  var URL = req.query.URL;
+  
+  try {
+
+      let info = await ytdl.getInfo(URL)
+
+      let filename = info.videoDetails.title+".mp4";
+      var title = info.videoDetails.title;
+
+      
+
+      res.writeHead(200, {
+        'Content-Type': 'application/force-download',
+        'Content-disposition':contentDisposition(filename)});
+
+
+           // Get audio and video stream going
+      const audio = ytdl(URL, { filter: 'audioonly', quality: 'highestaudio' })
+      const video = ytdl(URL, { filter: 'videoonly', quality: 'highestvideo' })
+      .once('end', () =>{
+        console.log('Finished Downloading')
+        
+        ffmpeg(`${title}1.mp4`)
+        .addInput(`${title}.m4a`)
+        .videoCodec('copy')
+        .audioCodec('copy')
+        .on("start", function(commandLine) {
+            console.log("Spawned FFmpeg with command: " + commandLine);
+        })
+        .on('error', function(err, stdout, stderr) {
+            console.log('Cannot process video: ' + err.message);
+        })
+        .on('end', function(stdout, stderr) {
+            console.log('Merging succeeded !');
+            fs.unlinkSync(__dirname + `/${title}.m4a`)
+            fs.unlinkSync(__dirname + `/${title}1.mp4`)
+
+
+            var file = fs.createReadStream(filename);
+            file.on('end', function() {
+              fs.unlink(filename, function() {
+                // file deleted
+              });
+            });
+            file.pipe(res);
+
+        })
+        .saveToFile(`${title}.mp4`)
+      })
+      
+      audio.pipe(fs.createWriteStream(__dirname + `/${title}.m4a`))
+      video.pipe(fs.createWriteStream(__dirname + `/${title}1.mp4`))
+  
+    } catch (err) {
+        console.log(err);
+    }
+
+
 })
